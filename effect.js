13c71c026da7a44074a3cc9ed653e23ebf33547d
atothejo <williammamen@gmail.com>
2019-05-15 02:49:28 +0700

    updet yuk
    kuy updet

--- effect.js
+++ effect.js
@@ -140,7 +140,7 @@ $('document').ready(function(){
 
 		
 	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
+		 vw = $(window).width()/100%;
 
 		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
 		$('#b1').attr('id','b11');
